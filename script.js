b0d33324d31b56582737b6df83d0bfd2b97e1876
Preshita <pandipreshita@gmail.com>
2021-06-06 01:03:01 +0800

    Commit message e.g Added signin

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//import Date
 // remember to add getter and setter methods where necessary
-
+// time is collected in 24 hours
+// how do we code to record a task that occupies for specific duration? --> naive idea: create /// a date object for every second, but this occupies way too much memory
+// need to determine the formats of the start_time and end_time
+// secs and millisecs not rly needed actually
 class Task {
     task_name;
     task_category;
@@ -37,7 +39,32 @@ class DailyTask extends RoutineTask {
     constructor(start_time, end_time) {
         super(start_time, end_time, "daily");
     }
-
+    
+    addTask(){
+        var i;
+        for (y = 0; y < Number.MAX_SAFE_INTEGER; y++) {
+            for (m = 0; m < 12; m++) {
+                // for feb
+                if (m == 1) {
+                    // need to create function to check if it is a leap year for feb
+                    for (d = 0; d < 29; d++) {
+                        let date = new Date(y, m, d, this.start_time.prototype.getHours(), this.start_time.prototype.getMins(), this.start_time.prototype.getSecs(), ms)
+                    }
+                // for apr, jun, sep, nov
+                } else if (m == 3 || 5 || 8 || 10) {
+                    for (d = 0; d < 31; d++) {
+                        let date = new Date(y, m, d, hours, mins, secs, ms)
+                    }
+                // for jan, mar, may, jul, aug, oct, dec
+                } else {
+                    for (d = 0; d < 32; d++) {
+                        let date = new Date(y, m, d, hours, mins, secs, ms)
+                    }
+                }
+                
+            }
+        }
+    }
     //for loop to add this task to each day --> unsure how to do a javascript for loop
 }
 

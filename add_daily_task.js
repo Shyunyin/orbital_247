10210473552f35f0f5bd0f8d612ba5d829703fa2
Shyun Yin <shyunyin321@gmail.com>
2021-06-06 21:53:41 +0800

    Edited css for add_daily_task

--- add_daily_task.js
+++ add_daily_task.js
@@ -17,3 +17,45 @@ function myFunction() {
       }
     }
   }
+
+/*CSS purpose: For selection of buttons to only happen once*/
+  function catFunction(button_switch){
+    if (button_switch === 1) {
+      document.querySelector("body div#categories li:last-child input#work").style.backgroundColor="white";
+      document.querySelector("body div#categories li:last-child input#exercise").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#categories li:last-child input#misc").style.backgroundColor="#e3aba1";
+    } else if (button_switch === 2) {
+      document.querySelector("body div#categories li:last-child input#work").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#categories li:last-child input#exercise").style.backgroundColor="white";
+      document.querySelector("body div#categories li:last-child input#misc").style.backgroundColor="#e3aba1";
+    } else if (button_switch === 3) {
+      document.querySelector("body div#categories li:last-child input#work").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#categories li:last-child input#exercise").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#categories li:last-child input#misc").style.backgroundColor="white";
+    } else {
+      document.querySelector("body div#categories li:last-child input#work").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#categories li:last-child input#exercise").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#categories li:last-child input#misc").style.backgroundColor="#e3aba1";
+    }
+  }
+
+/*CSS purpose: For selection of number of sessions to only 1*/
+  function sessionsFunction(click_switch){
+    if (click_switch === 1) {
+      document.querySelector("body div#sessions li:last-child input#onesession").style.backgroundColor="white";
+      document.querySelector("body div#sessions li:last-child input#twosessions").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#sessions li:last-child input#threesessions").style.backgroundColor="#e3aba1";
+    } else if (click_switch === 2) {
+      document.querySelector("body div#sessions li:last-child input#onesession").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#sessions li:last-child input#twosessions").style.backgroundColor="white";
+      document.querySelector("body div#sessions li:last-child input#threesessions").style.backgroundColor="#e3aba1";
+    } else if (click_switch === 3) {
+      document.querySelector("body div#sessions li:last-child input#onesession").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#sessions li:last-child input#twosessions").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#sessions li:last-child input#threesessions").style.backgroundColor="white";
+    } else {
+      document.querySelector("body div#sessions li:last-child input#onesession").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#sessions li:last-child input#twosessions").style.backgroundColor="#e3aba1";
+      document.querySelector("body div#sessions li:last-child input#threesessions").style.backgroundColor="#e3aba1";
+    }
+  }

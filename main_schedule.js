69553952f77aefdeb3c650cfe34de6a29b47a796
Shyun Yin <shyunyin321@gmail.com>
2021-06-23 00:00:35 +0800

    Created function for post it for main page

--- main_schedule.js
+++ main_schedule.js
@@ -46,3 +46,40 @@ window.onload  = function getHeading() {
     currentSchedule.setAttribute("value", day);
 }
 
+/*For popup*/
+function popup(mylink, windowname) {
+    if (! window.focus)return true;
+    var href;
+    if (typeof(mylink) == "string") href=mylink;
+    else href=mylink.href;
+    window.open(href, windowname, 'width=400, height=200, scrollbars=yes');
+    return false;
+}
+
+
+/*Must get input array of task names in the day, same as for add_daily_task page*/
+tasks = ["test1blahblah", "test2yadeyadeyade", "test3ldkmglkf"]; //find a way to get the tasks
+window.onload = function postit() {
+    for (let i = 0; i < tasks.length; i++) {
+        let append = document.createElement("input");
+        append.type="text"
+        append.readonly="readonly";
+        append.classList.add("task");
+        append.value=tasks[i]; //print out task
+        append.style.fontFamily="'Signika Negative', sans-serif";
+        append.style.fontSize="large";
+        append.style.position="absolute";
+        append.style.zIndex="2";
+        append.style.color="white";
+        append.style.backgroundColor="#1e5353";
+        append.style.border="none";
+        append.style.marginLeft="15px";
+        append.style.height="20px";
+        //calculation to ensure that tasks printed on top of each other
+        let top = i*30;
+        let topText = top + "px";
+        append.style.marginTop=topText;
+        let ele = document.getElementById("postit");
+        ele.appendChild(append);
+    }
+}

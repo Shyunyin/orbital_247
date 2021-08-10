fbc361346a76534bb344c79d33620d66b53d6042
Preshita01 <pandipreshita@gmail.com>
2021-08-10 11:49:11 +0800

    Edited insert statement to fit the new table

--- includes/add_to_DB.inc.php
+++ includes/add_to_DB.inc.php
@@ -13,9 +13,9 @@
     $startMin = (int) $_POST['currStartMin'];
     $endHour = (int) $_POST['currEndHour'];
     $endMin = (int) $_POST['currEndMin']; 
+    $completed = $_POST['currCompleted'];
     //$type = (int) $_POST['currCat']; //Type for fixed tasks is always 1
     //$userid = $_SESSION['userid'];
-    $type = $taskCat;
     //$userid = -1;
     $userid = $_SESSION["userid"];
 
@@ -24,7 +24,7 @@
     $db='orbital247';
     $conn = mysqli_connect('localhost', $user, $pass, $db);
 
-    $sql = "INSERT INTO fixedtaskwindow(taskName, taskCategory, taskYear, taskMonth, taskDate, startTimeHour, startTimeMin, endTimeHour, endTimeMin, taskType, userid) VALUES ('$taskName', $taskCat, $taskYear, $taskMonth, $taskDate, $startHour, $startMin, $endHour, $endMin, $type, $userid);";
+    $sql = "INSERT INTO fixedtaskwindow(taskName, taskCategory, taskYear, taskMonth, taskDate, startTimeHour, startTimeMin, endTimeHour, endTimeMin, completed, userid) VALUES ('$taskName', $taskCat, $taskYear, $taskMonth, $taskDate, $startHour, $startMin, $endHour, $endMin, $completed, $userid);";
 
     mysqli_query($conn, $sql);
 
